15ea5b12885a4e138243afda2b04f847763d4baf
Agustin <agustin.carrizo.5099@gmail.com>
2024-01-16 17:29:03 -0300

    Kinesiologia y pilates

--- src/assets/Components/Kinesiologia.jsx
+++ src/assets/Components/Kinesiologia.jsx
@@ -2,13 +2,121 @@ import React from "react";
 
 export default function Kinesiologia() {
    return (
-      <div className="flex justify-center items-center min-h-screen">
-         <div className="flex justify-center items-center w-auto h-auto bg-slate-400 text-black flex-col gap-6">
-            <h1 className="text-3xl mx-4">
-               Aca va a estar la pagina de Kinesiologia y Fisiatria
-            </h1>
-            <p>Estamos contstruyendolo</p>
+      <section className="flex items-center flex-col">
+         <div className=" w-3/4 text-justify  p-5 bg-slate-100 rounded-xl">
+            <h2 className="text-3xl ml-5 mb-4 ">Kinesiologia y fisiatria</h2>
+
+            <p className=" ">
+               Intervengo en las actividades vinculadas con los procesos de
+               evaluación y rehabilitación de las capacidades funcionales de las
+               personas, así como en la prevención, rehabilitación e integración
+               para lograr la plena inserción de la persona en la sociedad. Mi
+               trabajo en “Espacios” incluye tratamientos traumatológicos y
+               deportivos, terapia manual e instrumental, fisioterapia,
+               rehabilitación y terapia física en todas las edades, y también
+               trabajo postural. Las herramientas que me brinda este lugar de
+               trabajo son muchísimas, y gracias a eso el abanico terapéutico es
+               enorme.
+            </p>
          </div>
-      </div>
+         <section class="text-gray-600 body-font">
+            <div class="container px-5 py-24 mx-auto">
+               <div class="flex flex-col text-center w-full mb-20">
+                  <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">
+                     Resultados positivos de estos ejercicios
+                  </h1>
+               </div>
+               <div class="flex flex-wrap -m-4">
+                  <div class="p-4 md:w-1/3">
+                     <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                        <div class="flex items-center mb-3">
+                           <h2 class="text-gray-900 text-lg title-font font-medium">
+                              Shooting Stars
+                           </h2>
+                        </div>
+                        <div class="flex-grow">
+                           <p class="leading-relaxed text-base">
+                              Blue bottle crucifix vinyl post-ironic four dollar
+                              toast vegan taxidermy. Gastropub indxgo juice
+                              poutine.
+                           </p>
+                           <a class="mt-3 text-indigo-500 inline-flex items-center">
+                              Learn More
+                              <svg
+                                 fill="none"
+                                 stroke="currentColor"
+                                 stroke-linecap="round"
+                                 stroke-linejoin="round"
+                                 stroke-width="2"
+                                 class="w-4 h-4 ml-2"
+                                 viewBox="0 0 24 24">
+                                 <path d="M5 12h14M12 5l7 7-7 7"></path>
+                              </svg>
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="p-4 md:w-1/3">
+                     <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                        <div class="flex items-center mb-3">
+                           <h2 class="text-gray-900 text-lg title-font font-medium">
+                              The Catalyzer
+                           </h2>
+                        </div>
+                        <div class="flex-grow">
+                           <p class="leading-relaxed text-base">
+                              Blue bottle crucifix vinyl post-ironic four dollar
+                              toast vegan taxidermy. Gastropub indxgo juice
+                              poutine.
+                           </p>
+                           <a class="mt-3 text-indigo-500 inline-flex items-center">
+                              Learn More
+                              <svg
+                                 fill="none"
+                                 stroke="currentColor"
+                                 stroke-linecap="round"
+                                 stroke-linejoin="round"
+                                 stroke-width="2"
+                                 class="w-4 h-4 ml-2"
+                                 viewBox="0 0 24 24">
+                                 <path d="M5 12h14M12 5l7 7-7 7"></path>
+                              </svg>
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="p-4 md:w-1/3">
+                     <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                        <div class="flex items-center mb-3">
+                           <h2 class="text-gray-900 text-lg title-font font-medium">
+                              Neptune
+                           </h2>
+                        </div>
+                        <div class="flex-grow">
+                           <p class="leading-relaxed text-base">
+                              Blue bottle crucifix vinyl post-ironic four dollar
+                              toast vegan taxidermy. Gastropub indxgo juice
+                              poutine.
+                           </p>
+                           <a class="mt-3 text-indigo-500 inline-flex items-center">
+                              Learn More
+                              <svg
+                                 fill="none"
+                                 stroke="currentColor"
+                                 stroke-linecap="round"
+                                 stroke-linejoin="round"
+                                 stroke-width="2"
+                                 class="w-4 h-4 ml-2"
+                                 viewBox="0 0 24 24">
+                                 <path d="M5 12h14M12 5l7 7-7 7"></path>
+                              </svg>
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+      </section>
    );
 }
