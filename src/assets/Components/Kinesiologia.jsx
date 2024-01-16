f38402a43c7a494b2d05c6cf9c1d3fb14dc72ea2
Agustin <agustin.carrizo.5099@gmail.com>
2024-01-16 17:37:09 -0300

    trabajando en kinesiologia y fisio

--- src/assets/Components/Kinesiologia.jsx
+++ src/assets/Components/Kinesiologia.jsx
@@ -26,19 +26,18 @@ export default function Kinesiologia() {
                      Resultados positivos de estos ejercicios
                   </h1>
                </div>
-               <div class="flex flex-wrap -m-4">
-                  <div class="p-4 md:w-1/3">
-                     <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-                        <div class="flex items-center mb-3">
+               <div class="flex flex-wrap -m-4 ">
+                  <div class="p-4 md:w-1/3 ">
+                     <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col  items-center">
+                        <div class="flex items-center  mb-3">
                            <h2 class="text-gray-900 text-lg title-font font-medium">
-                              Shooting Stars
+                              Algo
                            </h2>
                         </div>
-                        <div class="flex-grow">
-                           <p class="leading-relaxed text-base">
-                              Blue bottle crucifix vinyl post-ironic four dollar
-                              toast vegan taxidermy. Gastropub indxgo juice
-                              poutine.
+                        <div class="flex flex-col items-center">
+                           <p class="leading-relaxed text-base text-center">
+                              Lorem ipsum dolor sit amet consectetur adipisicing
+                              elit. Ut.
                            </p>
                            <a class="mt-3 text-indigo-500 inline-flex items-center">
                               Learn More
@@ -57,60 +56,32 @@ export default function Kinesiologia() {
                      </div>
                   </div>
                   <div class="p-4 md:w-1/3">
-                     <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-                        <div class="flex items-center mb-3">
+                     <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col items-center">
+                        <div class="flex items-center mb-3 items">
                            <h2 class="text-gray-900 text-lg title-font font-medium">
-                              The Catalyzer
+                              algo.
                            </h2>
                         </div>
                         <div class="flex-grow">
-                           <p class="leading-relaxed text-base">
-                              Blue bottle crucifix vinyl post-ironic four dollar
-                              toast vegan taxidermy. Gastropub indxgo juice
-                              poutine.
+                           <p class="leading-relaxed text-base text-center">
+                              Lorem ipsum, dolor sit amet consectetur
+                              adipisicing.
                            </p>
-                           <a class="mt-3 text-indigo-500 inline-flex items-center">
-                              Learn More
-                              <svg
-                                 fill="none"
-                                 stroke="currentColor"
-                                 stroke-linecap="round"
-                                 stroke-linejoin="round"
-                                 stroke-width="2"
-                                 class="w-4 h-4 ml-2"
-                                 viewBox="0 0 24 24">
-                                 <path d="M5 12h14M12 5l7 7-7 7"></path>
-                              </svg>
-                           </a>
                         </div>
                      </div>
                   </div>
                   <div class="p-4 md:w-1/3">
-                     <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                     <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col items-center">
                         <div class="flex items-center mb-3">
                            <h2 class="text-gray-900 text-lg title-font font-medium">
-                              Neptune
+                              Algo
                            </h2>
                         </div>
                         <div class="flex-grow">
-                           <p class="leading-relaxed text-base">
-                              Blue bottle crucifix vinyl post-ironic four dollar
-                              toast vegan taxidermy. Gastropub indxgo juice
-                              poutine.
+                           <p class="leading-relaxed text-base text-center">
+                              Lorem ipsum dolor sit amet consectetur adipisicing
+                              elit. Id, similique asperiores!
                            </p>
-                           <a class="mt-3 text-indigo-500 inline-flex items-center">
-                              Learn More
-                              <svg
-                                 fill="none"
-                                 stroke="currentColor"
-                                 stroke-linecap="round"
-                                 stroke-linejoin="round"
-                                 stroke-width="2"
-                                 class="w-4 h-4 ml-2"
-                                 viewBox="0 0 24 24">
-                                 <path d="M5 12h14M12 5l7 7-7 7"></path>
-                              </svg>
-                           </a>
                         </div>
                      </div>
                   </div>
