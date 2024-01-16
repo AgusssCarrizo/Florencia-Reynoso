0b9e506474d19a1b3befdeb73a2e57db8b8b8225
Agustin <agustin.carrizo.5099@gmail.com>
2024-01-16 17:44:26 -0300

    Kine hero

--- src/assets/Components/Kinesiologia.jsx
+++ src/assets/Components/Kinesiologia.jsx
@@ -3,10 +3,12 @@ import React from "react";
 export default function Kinesiologia() {
    return (
       <section className="flex items-center flex-col">
-         <div className=" w-3/4 text-justify  p-5 bg-slate-100 rounded-xl">
-            <h2 className="text-3xl ml-5 mb-4 ">Kinesiologia y fisiatria</h2>
+         <div className="w-full px-20 md:w-3/4 text-justify p-5 bg-slate-600 md:bg-slate-100 md:rounded-xl md:dark:bg-slate-600 mt-16">
+            <h2 className="text-3xl text-center md:text-left mb-4 font-bold dark:text-slate-300">
+               Kinesiologia y fisiatria
+            </h2>
 
-            <p className=" ">
+            <p className=" font-medium dark:text-slate-200">
                Intervengo en las actividades vinculadas con los procesos de
                evaluación y rehabilitación de las capacidades funcionales de las
                personas, así como en la prevención, rehabilitación e integración
@@ -22,11 +24,11 @@ export default function Kinesiologia() {
          <section class="text-gray-600 body-font">
             <div class="container px-5 py-24 mx-auto">
                <div class="flex flex-col text-center w-full mb-20">
-                  <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">
+                  <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 dark:text-gray-50">
                      Resultados positivos de estos ejercicios
                   </h1>
                </div>
-               <div class="flex flex-wrap -m-4 ">
+               <div class="flex flex-wrap justify-center -m-4 ">
                   <div class="p-4 md:w-1/3 ">
                      <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col  items-center">
                         <div class="flex items-center  mb-3">
