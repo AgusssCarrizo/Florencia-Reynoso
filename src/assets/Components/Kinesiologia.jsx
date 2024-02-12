2e76145d96d2e33bfe66ec3ba9c3de0753507d44
Agustin <agustin.carrizo.5099@gmail.com>
2024-02-12 18:38:38 -0300

    Carrousel

--- src/assets/Components/Kinesiologia.jsx
+++ src/assets/Components/Kinesiologia.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Kinesiologia() {
    return (
       <section className="flex items-center flex-col">
-         <div className="w-full px-20 md:w-3/4 text-justify p-5 bg-slate-600 md:bg-slate-100 md:rounded-xl md:dark:bg-slate-600 mt-16">
+         <div className="w-full px-20 md:w-3/4 text-justify p-5 bg-slate-200  md:rounded-xl md:dark:bg-slate-600 mt-16">
             <h2 className="text-3xl text-center md:text-left mb-4 font-bold dark:text-slate-300">
                Kinesiologia y fisiatria
             </h2>
@@ -15,14 +15,14 @@ export default function Kinesiologia() {
                lograr la plena inserción de la persona en la sociedad.
             </p>
          </div>
-         <section class="text-gray-600 body-font w-[screen] h-[500px]">
+         <section class="text-gray-600 body-font w-screen flex flex-col justify-center h-[500px]">
             <div class="container px-5 py-24 mx-auto">
                <div class="flex flex-col text-center w-full mb-20">
                   <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 dark:text-gray-50">
                      Servicios
                   </h1>
                </div>
-               <div className="carousel w-svw lg:w-full h-60">
+               <div className="carousel w-full h-60">
                   <div
                      id="slide1"
                      className="carousel-item relative w-full gap-4">
