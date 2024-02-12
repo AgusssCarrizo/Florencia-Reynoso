8318dcc44a7e9f61459dbc7ba3343d6127953db4
Agustin <agustin.carrizo.5099@gmail.com>
2024-02-12 17:43:50 -0300

    Kine

--- src/assets/Components/Actividades.jsx
+++ src/assets/Components/Actividades.jsx
@@ -2,27 +2,44 @@ import React from "react";
 
 export default function Actividades() {
    return (
-      <div className="grid grid-cols-2 lg:grid-cols-3 w-5/6 mx-auto gap-6">
-         <div>
+      <div className="grid grid-flow-dense lg:grid-cols-3 w-5/6 mx-auto gap-6 items-center ">
+         <div className="rounded-lg my-3 ">
             <img
                src="src/assets/img/Pilates/Circuito.jpeg"
-               alt="Fisioterapia"
+               alt="Pilates circuito"
+               className="w-full rounded-t"
             />
+            <h4 className="text-sm bg-green-200 h-6 rounded-b">
+               Pilates circuito
+            </h4>
          </div>
-         <div>
+         <div className="my-3  ">
             <img
                src="src/assets/img/Pilates/Pilates-embarazadas.jpeg"
-               alt="Fisioterapia"
+               alt="Pilates para embarazadas"
+               className="w-full h-full rounded-t "
             />
+            <h4 className="text-sm bg-green-200 h-6 rounded-b">
+               Pilates para embarazadas
+            </h4>
          </div>
-         <div>
+         <div className="my-3 ">
             <img
                src="src/assets/img/Pilates/Pilates-mix.jpg"
-               alt="Fisioterapia"
+               alt="Pilates mix"
+               className="  h-full rounded-t"
             />
+            <h4 className="text-sm bg-green-200 h-6 rounded-b">Pilates mix</h4>
          </div>
-         <div>
-            <img src="src/assets/img/Pilates/Reformer.jpg" alt="Fisioterapia" />
+         <div className="grid col-start-2 col-end-3 my-3">
+            <img
+               src="src/assets/img/Pilates/Reformer.jpg"
+               alt="Pilates reformer"
+               className="w-full  h-full rounded-t"
+            />
+            <h4 className="text-sm  bg-green-200 h-6 rounded-b">
+               Pilates reformer
+            </h4>
          </div>
       </div>
    );
