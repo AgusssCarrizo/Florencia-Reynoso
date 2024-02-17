b2b1412a61a6c567665421471d3a4a7022e26064
Agustin <agustin.carrizo.5099@gmail.com>
2024-02-17 03:12:23 -0300

    cosas3

--- src/assets/Components/Kinesiologia.jsx
+++ src/assets/Components/Kinesiologia.jsx
@@ -173,7 +173,7 @@ export default function Kinesiologia() {
                            </div>
                         </div>
                      </div>
-                     <div className="flex rounded-lg h-full w-1/3 sm:w-1/2 bg-gradient-to-r from-emerald-700 to-emerald-600   p-8 flex-col  items-center justify-center text-center ">
+                     <div className="flex rounded-lg h-full w-1/3 sm:w-1/2 bg-gradient-to-r from-emerald-700 to-emerald-600   flex-col  items-center text-center justify-center gap-7 ">
                         <h3 className="text-slate-200 text-sm md:text-lg font-bold  ">
                            Terapia con ventosas.
                         </h3>
