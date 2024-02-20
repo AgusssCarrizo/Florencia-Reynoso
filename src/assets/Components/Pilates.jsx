2730b1ccf663c09fa6a990497ee6c5bcc53bc7ce
Agustin <agustin.carrizo.5099@gmail.com>
2024-02-20 18:02:33 -0300

    colors pilates

--- src/assets/Components/Pilates.jsx
+++ src/assets/Components/Pilates.jsx
@@ -3,7 +3,7 @@ import Actividades from "./Actividades";
 export default function Kinesiologia() {
    return (
       <section className="flex items-center flex-col">
-         <div className="w-full px-20 md:w-3/4 text-justify p-5 bg-slate-600 md:bg-slate-100 md:rounded-xl md:dark:bg-slate-600 mt-16">
+         <div className="w-full px-20 md:w-3/4 text-justify p-5 bg-slate-100 md:rounded-xl md:dark:bg-slate-600 mt-16">
             <h2 className="text-3xl text-center md:text-left mb-4 font-bold dark:text-slate-300">
                Pilates
             </h2>
