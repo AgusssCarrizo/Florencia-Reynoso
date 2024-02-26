05a7cd654c069659bacd1d55ccbad2e0e2a2bdcf
Agustin <agustin.carrizo.5099@gmail.com>
2024-02-26 13:14:33 -0300

    map

--- src/assets/Components/Ubicacion.jsx
+++ src/assets/Components/Ubicacion.jsx
@@ -4,9 +4,9 @@ export default function Ubicacion() {
    return (
       <div className="h-80 w-full flex justify-center gap-3 items-center mx-auto mb-6 flex-col md:flex-row">
          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3267.061583540665!2d-63.01563875582889!3d-35.03018923817866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95c43ad55c30078d%3A0x65d23122115d6c93!2sCastelli%20672!5e0!3m2!1ses-419!2sar!4v1708462138000!5m2!1ses-419!2sar"
-            width="300"
-            height="300"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3267.0540224765728!2d-63.015700599999995!3d-35.030378400000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95c43ad55883a321%3A0x4a8f37ddf44f5e00!2sCastelli%20672%2C%20Gral.%20Villegas%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1708963933840!5m2!1ses-419!2sar"
+            width="600"
+            height="450"
             style={{border: 0}}
             allowfullscreen=""
             loading="lazy"
